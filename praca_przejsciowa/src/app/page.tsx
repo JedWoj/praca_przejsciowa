9354dd21c60468da283f06263e89aa18a1b4e6cb
JedWoj <jedwojc@gmail.com>
2024-04-04 00:04:15 +0200

    Added Virtualization + static data

--- praca_przejsciowa/src/app/page.tsx
+++ praca_przejsciowa/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Table } from "./components/Table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen">
       <Table />
     </main>
   );
---
+++ praca_przejsciowa/src/app/utils/types.ts
@@ -0,0 +1,7 @@
+export type Item = {
+  name: string;
+  currentStock: number;
+  price: number;
+  lastOrder: string;
+  location: string;
+};
