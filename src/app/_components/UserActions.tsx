2e9dde50a2621de8ee8e5fa4965821270891669d
JedWoj <jedwojc@gmail.com>
2024-09-20 13:17:26 +0200

    ops: preview deploy

--- src/app/_components/UserActions.tsx
+++ src/app/_components/UserActions.tsx
@@ -6,13 +6,13 @@ export function UserActions() {
   return (
     <section className="flex gap-4">
       <Link href="/delivery">
-        <UserAction>Handle deliveries</UserAction>
+        <UserAction>Handle deliveries!</UserAction>
       </Link>
       <Link href="/storage">
-        <UserAction>Check storage state</UserAction>
+        <UserAction>Check storage state!</UserAction>
       </Link>
       <Link href="/overview">
-        <UserAction>Items overview</UserAction>
+        <UserAction>Items overview!</UserAction>
       </Link>
     </section>
   );
